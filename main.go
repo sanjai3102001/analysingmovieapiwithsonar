82c67e3ad910d4560c0344d94a21fb2eaff34004
sanjai3102001 <98545323+sanjai3102001@users.noreply.github.com>
2022-09-30 10:44:06 +0530

    commented the duplicated lines

--- main.go
+++ main.go
@@ -1,277 +1,265 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
+// var accesskey string = "AKIASA45Q7S6M3LEBBL6"
+// var reagion string = "ap-northeast-1"
+// var secret string = "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"
+// var tableName string = "Movies"
+// var Title string = "kgf3"
+// var movieid string = "2010"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-)
+// // This CreateItem function id used to create a item in a dynamo db
+// func CreateItem(w http.ResponseWriter, r *http.Request) {
 
-var accesskey string = "AKIASA45Q7S6M3LEBBL6"
-var reagion string = "ap-northeast-1"
-var secret string = "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"
-var tableName string = "Movies"
-var Title string = "kgf3"
-var movieid string = "2010"
+// 	sess, _ := session.NewSession(&aws.Config{
+// 		Region:      aws.String(reagion),
+// 		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
+// 	})
+// 	fmt.Println(sess.Config.Credentials.Get())
+// 	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
 
-// This CreateItem function id used to create a item in a dynamo db
-func CreateItem(w http.ResponseWriter, r *http.Request) {
+// 	item := map[string]interface{}{
+// 		"Movieid":  2022,
+// 		"Title":    "kgf2",
+// 		"Hero":     "yash",
+// 		"isactive": true,
+// 	}
 
-	sess, _ := session.NewSession(&aws.Config{
-		Region:      aws.String(reagion),
-		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
-	})
-	fmt.Println(sess.Config.Credentials.Get())
-	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
+// 	av, err := dynamodbattribute.MarshalMap(item)
+// 	if err != nil {
+// 		log.Fatalf("Got error marshalling new movie item: %s", err)
+// 	}
 
-	item := map[string]interface{}{
-		"Movieid":  2022,
-		"Title":    "kgf2",
-		"Hero":     "yash",
-		"isactive": true,
-	}
+// 	tableName := "Movies"
 
-	av, err := dynamodbattribute.MarshalMap(item)
-	if err != nil {
-		log.Fatalf("Got error marshalling new movie item: %s", err)
-	}
+// 	input := &dynamodb.PutItemInput{
+// 		Item:      av,
+// 		TableName: aws.String(tableName),
+// 	}
 
-	tableName := "Movies"
+// 	_, err = svc.PutItem(input)
+// 	if err != nil {
+// 		log.Fatalf("Got error calling PutItem: %s", err)
+// 	}
 
-	input := &dynamodb.PutItemInput{
-		Item:      av,
-		TableName: aws.String(tableName),
-	}
+// 	// year := strconv.Itoa(item["Movieid"].(int))
 
-	_, err = svc.PutItem(input)
-	if err != nil {
-		log.Fatalf("Got error calling PutItem: %s", err)
-	}
+// 	// fmt.Println("Successfully added '" + item["Title"].(string) + "' (" + year + ") to table " + tableName)
+// }
 
-	// year := strconv.Itoa(item["Movieid"].(int))
+// type Item struct {
+// 	Movieid int
+// 	Title   string
+// 	Hero    string
+// }
 
-	// fmt.Println("Successfully added '" + item["Title"].(string) + "' (" + year + ") to table " + tableName)
-}
+// // This ReadingItem function id used to read a item in a dynamo db
+// func ReadingItem(w http.ResponseWriter, r *http.Request) {
 
-type Item struct {
-	Movieid int
-	Title   string
-	Hero    string
-}
+// 	sess, _ := session.NewSession(&aws.Config{
+// 		Region:      aws.String(reagion),
+// 		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
+// 	})
+// 	fmt.Println(sess.Config.Credentials.Get())
+// 	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
 
-// This ReadingItem function id used to read a item in a dynamo db
-func ReadingItem(w http.ResponseWriter, r *http.Request) {
+// 	tableName := "Movies"
+// 	Title := "kgf"
+// 	movieid := "2010"
 
-	sess, _ := session.NewSession(&aws.Config{
-		Region:      aws.String(reagion),
-		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
-	})
-	fmt.Println(sess.Config.Credentials.Get())
-	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
+// 	result, err := svc.GetItem(&dynamodb.GetItemInput{
+// 		TableName: aws.String(tableName),
+// 		Key: map[string]*dynamodb.AttributeValue{
+// 			"Movieid": {
+// 				N: aws.String(movieid),
+// 			},
+// 			"Title": {
+// 				S: aws.String(Title),
+// 			},
+// 		},
+// 	})
+// 	fmt.Println(result)
+// 	if err != nil {
+// 		log.Fatalf("Got error calling GetItem: %s", err)
+// 	}
+// 	// id, err := strconv.Atoi(movieid)
 
-	tableName := "Movies"
-	Title := "kgf"
-	movieid := "2010"
+// 	// if id != 0 {
+// 	// 	log.Fatalf("No item: %s", err)
+// 	// }
 
-	result, err := svc.GetItem(&dynamodb.GetItemInput{
-		TableName: aws.String(tableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"Movieid": {
-				N: aws.String(movieid),
-			},
-			"Title": {
-				S: aws.String(Title),
-			},
-		},
-	})
-	fmt.Println(result)
-	if err != nil {
-		log.Fatalf("Got error calling GetItem: %s", err)
-	}
-	// id, err := strconv.Atoi(movieid)
+// 	item := Item{}
 
-	// if id != 0 {
-	// 	log.Fatalf("No item: %s", err)
-	// }
+// 	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+// 	if err != nil {
+// 		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+// 	}
 
-	item := Item{}
+// 	fmt.Println("Found item:")
+// 	fmt.Println("Id:  ", item.Movieid)
+// 	fmt.Println("Title: ", item.Title)
+// 	fmt.Println("Hero:  ", item.Hero)
 
-	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
-	}
+// }
 
-	fmt.Println("Found item:")
-	fmt.Println("Id:  ", item.Movieid)
-	fmt.Println("Title: ", item.Title)
-	fmt.Println("Hero:  ", item.Hero)
+// // This ReadItemId function is used to read the specfic item from an dynamo db
+// func ReadingItemid(w http.ResponseWriter, r *http.Request) {
+// 	sess, _ := session.NewSession(&aws.Config{
+// 		Region:      aws.String(reagion),
+// 		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
+// 	})
+// 	fmt.Println(sess.Config.Credentials.Get())
+// 	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
 
-}
+// 	// tableName := "Movies"
+// 	// Title := "kgf3"
+// 	// movieid := "2010"
 
-// This ReadItemId function is used to read the specfic item from an dynamo db
-func ReadingItemid(w http.ResponseWriter, r *http.Request) {
-	sess, _ := session.NewSession(&aws.Config{
-		Region:      aws.String(reagion),
-		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
-	})
-	fmt.Println(sess.Config.Credentials.Get())
-	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
+// 	result, err := svc.GetItem(&dynamodb.GetItemInput{
+// 		TableName: aws.String(tableName),
+// 		Key: map[string]*dynamodb.AttributeValue{
+// 			"Movieid": {
+// 				N: aws.String(movieid),
+// 			},
+// 			"Title": {
+// 				S: aws.String(Title),
+// 			},
+// 		},
+// 	})
+// 	fmt.Println(result)
+// 	if err != nil {
+// 		log.Fatalf("Got error calling GetItem: %s", err)
+// 	}
+// 	//id, err := strconv.Atoi(movieid)
 
-	// tableName := "Movies"
-	// Title := "kgf3"
-	// movieid := "2010"
+// 	if Title == "" {
+// 		log.Fatalf("No item: %s", err)
+// 	}
 
-	result, err := svc.GetItem(&dynamodb.GetItemInput{
-		TableName: aws.String(tableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"Movieid": {
-				N: aws.String(movieid),
-			},
-			"Title": {
-				S: aws.String(Title),
-			},
-		},
-	})
-	fmt.Println(result)
-	if err != nil {
-		log.Fatalf("Got error calling GetItem: %s", err)
-	}
-	//id, err := strconv.Atoi(movieid)
+// 	item := Item{}
 
-	if Title == "" {
-		log.Fatalf("No item: %s", err)
-	}
+// 	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+// 	if err != nil {
+// 		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+// 	}
 
-	item := Item{}
+// 	fmt.Println("Found item:")
+// 	fmt.Println("Id:  ", item.Movieid)
+// 	fmt.Println("Title: ", item.Title)
+// 	fmt.Println("Hero:  ", item.Hero)
 
-	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
-	}
+// }
 
-	fmt.Println("Found item:")
-	fmt.Println("Id:  ", item.Movieid)
-	fmt.Println("Title: ", item.Title)
-	fmt.Println("Hero:  ", item.Hero)
+// // This Softdelete function id used to delete a item in a dynamo db
+// func Softdelete(w http.ResponseWriter, r *http.Request) {
 
-}
+// 	sess, _ := session.NewSession(&aws.Config{
+// 		Region:      aws.String(reagion),
+// 		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
+// 	})
+// 	fmt.Println(sess.Config.Credentials.Get())
+// 	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
 
-// This Softdelete function id used to delete a item in a dynamo db
-func Softdelete(w http.ResponseWriter, r *http.Request) {
+// 	// tableName := "Movies"
+// 	// Title := "kgf2"
+// 	Movieid := "2022"
+// 	Isactive := false
 
-	sess, _ := session.NewSession(&aws.Config{
-		Region:      aws.String(reagion),
-		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
-	})
-	fmt.Println(sess.Config.Credentials.Get())
-	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
+// 	input := &dynamodb.UpdateItemInput{
+// 		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+// 			":r": {
+// 				BOOL: aws.Bool(Isactive),
+// 			},
+// 		},
+// 		TableName: aws.String(tableName),
+// 		Key: map[string]*dynamodb.AttributeValue{
+// 			"Movieid": {
+// 				N: aws.String(Movieid),
+// 			},
+// 			"Title": {
+// 				S: aws.String(Title),
+// 			},
+// 		},
+// 		ReturnValues:     aws.String("UPDATED_NEW"),
+// 		UpdateExpression: aws.String("set Isactive = :r"),
+// 	}
 
-	// tableName := "Movies"
-	// Title := "kgf2"
-	Movieid := "2022"
-	Isactive := false
+// 	_, err := svc.UpdateItem(input)
+// 	if err != nil {
+// 		log.Fatalf("Got error calling UpdateItem: %s", err)
+// 	}
 
-	input := &dynamodb.UpdateItemInput{
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":r": {
-				BOOL: aws.Bool(Isactive),
-			},
-		},
-		TableName: aws.String(tableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"Movieid": {
-				N: aws.String(Movieid),
-			},
-			"Title": {
-				S: aws.String(Title),
-			},
-		},
-		ReturnValues:     aws.String("UPDATED_NEW"),
-		UpdateExpression: aws.String("set Isactive = :r"),
-	}
+// }
 
-	_, err := svc.UpdateItem(input)
-	if err != nil {
-		log.Fatalf("Got error calling UpdateItem: %s", err)
-	}
+// // This is a update function for a movie api
+// func UpdateItems(w http.ResponseWriter, r *http.Request) {
 
-}
+// 	sess, _ := session.NewSession(&aws.Config{
+// 		Region:      aws.String(reagion),
+// 		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
+// 	})
+// 	fmt.Println(sess.Config.Credentials.Get())
+// 	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
 
-// This is a update function for a movie api
-func UpdateItems(w http.ResponseWriter, r *http.Request) {
+// 	// tableName := "Movies"
+// 	// Title := "kgf2"
+// 	Movieid := "2010"
+// 	Hero := "kgdyash"
 
-	sess, _ := session.NewSession(&aws.Config{
-		Region:      aws.String(reagion),
-		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
-	})
-	fmt.Println(sess.Config.Credentials.Get())
-	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
+// 	input := &dynamodb.UpdateItemInput{
+// 		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+// 			":r": {
+// 				S: aws.String(Hero),
+// 			},
+// 		},
+// 		TableName: aws.String(tableName),
+// 		Key: map[string]*dynamodb.AttributeValue{
+// 			"Movieid": {
+// 				N: aws.String(Movieid),
+// 			},
+// 			"Title": {
+// 				S: aws.String(Title),
+// 			},
+// 		},
+// 		ReturnValues:     aws.String("UPDATED_NEW"),
+// 		UpdateExpression: aws.String("set Hero = :r"),
+// 	}
 
-	// tableName := "Movies"
-	// Title := "kgf2"
-	Movieid := "2010"
-	Hero := "kgdyash"
+// 	_, err := svc.UpdateItem(input)
+// 	if err != nil {
+// 		log.Fatalf("Got error calling UpdateItem: %s", err)
+// 	}
 
-	input := &dynamodb.UpdateItemInput{
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":r": {
-				S: aws.String(Hero),
-			},
-		},
-		TableName: aws.String(tableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"Movieid": {
-				N: aws.String(Movieid),
-			},
-			"Title": {
-				S: aws.String(Title),
-			},
-		},
-		ReturnValues:     aws.String("UPDATED_NEW"),
-		UpdateExpression: aws.String("set Hero = :r"),
-	}
+// 	fmt.Println("Successfully updated '" + Title + "' (" + Movieid + ") rating to " + Title)
 
-	_, err := svc.UpdateItem(input)
-	if err != nil {
-		log.Fatalf("Got error calling UpdateItem: %s", err)
-	}
+// }
 
-	fmt.Println("Successfully updated '" + Title + "' (" + Movieid + ") rating to " + Title)
+// func DeleteItem(w http.ResponseWriter, r *http.Request) {
 
-}
+// 	sess, _ := session.NewSession(&aws.Config{
+// 		Region:      aws.String(reagion),
+// 		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
+// 	})
+// 	fmt.Println(sess.Config.Credentials.Get())
+// 	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
 
-func DeleteItem(w http.ResponseWriter, r *http.Request) {
+// 	// tableName := "Movies"
+// 	movieName := "xyz"
+// 	// movieid := "2001"
 
-	sess, _ := session.NewSession(&aws.Config{
-		Region:      aws.String(reagion),
-		Credentials: credentials.NewStaticCredentials(accesskey, secret, ""),
-	})
-	fmt.Println(sess.Config.Credentials.Get())
-	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("")})
-
-	// tableName := "Movies"
-	movieName := "xyz"
-	// movieid := "2001"
-
-	input := &dynamodb.DeleteItemInput{
-		Key: map[string]*dynamodb.AttributeValue{
-			"Movieid": {
-				N: aws.String(movieid),
-			},
-			"Title": {
-				S: aws.String(movieName),
-			},
-		},
-		TableName: aws.String(tableName),
-	}
-	_, err := svc.DeleteItem(input)
-	if err != nil {
-		log.Fatalf("Got error calling DeleteItem:%s", err)
-	}
-	fmt.Println("Deleted'" + movieName + "'(" + movieid + ")from table" + tableName)
-}
+// 	input := &dynamodb.DeleteItemInput{
+// 		Key: map[string]*dynamodb.AttributeValue{
+// 			"Movieid": {
+// 				N: aws.String(movieid),
+// 			},
+// 			"Title": {
+// 				S: aws.String(movieName),
+// 			},
+// 		},
+// 		TableName: aws.String(tableName),
+// 	}
+// 	_, err := svc.DeleteItem(input)
+// 	if err != nil {
+// 		log.Fatalf("Got error calling DeleteItem:%s", err)
+// 	}
+// 	fmt.Println("Deleted'" + movieName + "'(" + movieid + ")from table" + tableName)
+// }
